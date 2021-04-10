c01ccf5cd4c34aa78501f4f789ef15de25c5b074
Thiago Moura <thmoura14@hotmail.com>
2021-04-10 10:34:59 -0300

    Go Simulator app

---
+++ simulator/application/kafka/produce.go
@@ -0,0 +1,28 @@
+package kafka
+
+import (
+	"os"
+	"log"
+	"time"
+	"encoding/json"
+	"github.com/thienry/go-simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	router "github.com/thienry/go-simulator/application/route"
+)
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := router.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}  
+}
---
+++ simulator/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/joho/godotenv"
+	"github.com/thienry/go-simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	kafka2 "github.com/thienry/go-simulator/application/kafka"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
+}
